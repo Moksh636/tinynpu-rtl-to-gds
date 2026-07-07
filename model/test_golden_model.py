0993a76d1e2cb9b2bbbfc5451595801df534a7ad
Moksh636 <mokshpat636@gmail.com>
2026-07-06 22:27:39 -0500

    Add Python golden model and project test target

--- model/test_golden_model.py
+++ model/test_golden_model.py
@@ -36,7 +36,12 @@ def test_flatten_4x4():
         [13, 14, 15, 16],
     ]
 
-    assert flatten_4x4(m) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    assert flatten_4x4(m) == [
+        1, 2, 3, 4,
+        5, 6, 7, 8,
+        9, 10, 11, 12,
+        13, 14, 15, 16,
+    ]
 
 
 def test_random_matrix_shape():
